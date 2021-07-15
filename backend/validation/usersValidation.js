3374110c5f73a48960691bc1ff95760a6a420351
garvitgupta13 <garvitgupta2001@gmail.com>
2021-07-16 00:14:52 +0530

    returning error after validation

--- backend/validation/usersValidation.js
+++ backend/validation/usersValidation.js
@@ -36,34 +36,34 @@ const Joi = require('joi')
 // })
 
 
+function validateSignUp(data) {
 
-const schema = Joi.object({
-    name: Joi.string().min(2).required(),
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required().strict(),
-    confirmPassword: Joi.string().valid(Joi.ref('password')).required().strict(),
-    age: Joi.number().min(2).max(100),
-    height: Joi.number().integer(),
-    weight: Joi.number().integer(),
-    bloodType: Joi.string().valid('A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-'),
-    education: Joi.string().allow(''),
-    diseaseDescription: Joi.string().empty('').allow(null),
-    prescription: Joi.string(),
-    recommendedTests: Joi.string().empty('').allow(null),
-    domain: Joi.string(),
-    yearsOfExperience: Joi.number().integer(),
-    education: Joi.string().allow(''),
-    timeSlot: Joi.array().items(
-        Joi.object({
-            startTime: Joi.string(),
-            endTime: Joi.string()
-        })
+    const schema = Joi.object({
+        name: Joi.string().min(2).required(),
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required().strict(),
+        confirmPassword: Joi.string().valid(Joi.ref('password')).required().strict(),
+        age: Joi.number().min(2).max(100),
+        height: Joi.number().integer(),
+        weight: Joi.number().integer(),
+        bloodType: Joi.string().valid('A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-'),
+        education: Joi.string().allow(''),
+        diseaseDescription: Joi.string().empty('').allow(null),
+        prescription: Joi.string(),
+        recommendedTests: Joi.string().empty('').allow(null),
+        domain: Joi.string(),
+        yearsOfExperience: Joi.number().integer(),
+        education: Joi.string().allow(''),
+        bio: Joi.string().allow(''),
+        startTime: Joi.string(),
+        endTime: Joi.string(),
+        documentImage: Joi.string()
+    })
+    const { error, value } = schema.validate(data);
+    return error;
+}
 
-    ),
-    documentImage: Joi.string()
-})
-
-const validateSignUp = (data) => schema.validate(data)
+// const validateSignUp = (data) => schema.validate(data)
 
 
 const validateSignIn = (data) => {
