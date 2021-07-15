3ca653e4f534129bcf5d3a2ab7eb065d3b5eb3f5
garvitgupta13 <garvitgupta2001@gmail.com>
2021-07-16 00:12:47 +0530

    adding patient routes

--- backend/routes/patientRoute.js
+++ backend/routes/patientRoute.js
@@ -1,16 +1,13 @@
 const router = require("express").Router();
-const {
-  getPatients,
-  getPatient,
-  updatePatient
-} = require("../controllers/patientController");
+const { getPatients, getPatient, updatePatient } = require("../controllers/patientController");
 
 //! Get all patients
-router.get("/", getPatients);
+router.get('/', getPatients);
 
 //! Get a patient
-router.get("/:id", getPatient);
+router.get('/:id', getPatient);
 
-router.put("/:id", updatePatient);
+//! Update a patient
+router.put('/:id', updatePatient);
 
 module.exports = router;
