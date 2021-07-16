87f15e368abf2d239fb266a72380b4eb1d37256c
Garvit Gupta <garvitgupta2001@gmail.com>
2021-07-16 23:23:30 +0530

    resolving merge conflicts

--- backend/routes/doctorRoutes.js
+++ backend/routes/doctorRoutes.js
@@ -1,11 +1,6 @@
 const router = require("express").Router();
-<<<<<<< HEAD
-const { getDoctors, getDoctor, updateDoctor, addReview } = require("../controllers/doctorController");
 const authVerify = require('../middleware/authVerify')
-
-=======
 const { getDoctors, getDoctor, updateDoctor, addReview, addConsultation } = require("../controllers/doctorController");
->>>>>>> f7345c8d8ff57facccbe26ef9b42f3d361469b99
 
 //! Get all doctor
 router.get('/',authVerify, getDoctors);
@@ -16,12 +11,8 @@ router.get('/:id', authVerify, getDoctor);
 //! Update a doctor
 router.put('/:id', authVerify, updateDoctor);
 
-<<<<<<< HEAD
-router.post('/review/:id',authVerify, addReview);
-=======
 //! Add review
 router.post('/review/:id', addReview);
->>>>>>> f7345c8d8ff57facccbe26ef9b42f3d361469b99
 
 //!Add consultation
 router.put('/consultation/:doctorId/:patientId', addConsultation);
