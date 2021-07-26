bc0c382de1c80f3a9beb96ffce0544fb269f19a0
garvitgupta13 <garvitgupta2001@gmail.com>
2021-07-26 17:30:32 +0530

    🎉we are finally processing payments

--- frontend/src/Services/paymentServices.js
+++ frontend/src/Services/paymentServices.js
@@ -12,35 +12,13 @@ export const getmeToken = async (token) => {
 }
 
 export const processPayment = async (paymentData, token) => {
-    //FIX: Not working (Unable to fetch data from api)
-    /*try {
-        const header = {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-            "X-Requested-With": "XMLHttpRequest"
-        };
-
+    try {
         axios.defaults.headers.common["Authorization"] = token;
-        const response = await axios.post(`${process.env.REACT_APP_API_ENDPOINT}/payment/`, paymentData, header);
+        const response = await axios.post(`${process.env.REACT_APP_API_ENDPOINT}/payment/`, paymentData);
         console.log("response: ", response);
         return response;
     } catch (error) {
         console.log("error: ", error);
-    }*/
-
-    return fetch(`${process.env.REACT_APP_API_ENDPOINT}/payment/`, {
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            "Authorization": token,
-            "X-Requested-With": "XMLHttpRequest"
-        },
-        body: paymentData
-    })
-        .then((response) => {
-            return response;
-        })
-        .catch((err) => console.log(err));
+    }
 }
 
