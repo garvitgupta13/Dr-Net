219a765b13904839f58f4bfe71c90ce11684439e
garvitgupta13 <garvitgupta2001@gmail.com>
2021-08-30 17:09:52 +0530

    modifying the message controller

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,5 +1,6 @@
 const Conversation = require('../models/Conversations');
 const Message = require('../models/Message');
+const _ = require('lodash');
 
 const addMessage = async (req, res) => {
   try {
@@ -7,39 +8,37 @@ const addMessage = async (req, res) => {
     message.senderId = req.user._id;
     message.conversationId = req.params.conversationId;
 
-    let conversation = await Conversation.findById(req.params.conversationId);
+    let { canChat, patient, doctor } = await Conversation.findById(req.params.conversationId);
 
-    if (!conversation.canChat || !conversation.recipients.includes(req.user._id)) {
-      return res.send({
-        status: 500,
-        message: "ACESS DENIED"
-      })
+    if (!canChat || (!_.isEqual(patient._id, req.user._id) && !_.isEqual(doctor._id, req.user._id))) {
+      return res.status(401).send('ACCESS DENIED');
     }
 
     let newMessage = new Message(message);
     await newMessage.save();
+
     return res.send(newMessage);
   } catch (err) {
     console.log(err);
-    res.status(500).send(err);
+    res.status(500).send(err.message);
   }
 };
 
 const getMessages = async (req, res) => {
   try {
-    const conversation = await Conversation.findById(req.params.conversationId);
-    if (!conversation.recipients.includes(req.user._id)) {
+    const { patient, doctor } = await Conversation.findById(req.params.conversationId);
+    if (!_.isEqual(patient._id, req.user._id) && !_.isEqual(doctor._id, req.user._id)) {
       return res.send({
-        status: 500,
-        message: "ACESS DENIED"
-      })
+        status: 401,
+        message: 'ACESS DENIED',
+      });
     }
     const messages = await Message.find({
       conversationId: req.params.conversationId,
     });
     res.status(200).json(messages);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err.message);
   }
 };
 
