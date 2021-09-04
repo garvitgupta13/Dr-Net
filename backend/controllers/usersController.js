305f6ac23fe01554d86b7ec0a6b7c8dda9778814
garvitgupta13 <garvitgupta2001@gmail.com>
2021-09-04 18:07:55 +0530

    fixing the user signin bug

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,16 +1,13 @@
-const User = require("../models/Users");
-const Patient = require("../models/Patients");
-const Doctor = require("../models/Doctors");
-const jwt = require("jsonwebtoken");
-const _ = require("lodash")
-const bcrypt = require("bcrypt");
-const jwtGenerator = require("../utils/jwtGenerator");
-require("dotenv").config();
+const User = require('../models/Users');
+const Patient = require('../models/Patients');
+const Doctor = require('../models/Doctors');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
+const jwtGenerator = require('../utils/jwtGenerator');
+require('dotenv').config();
 
-const {
-  validateSignUp,
-  validateSignIn
-} = require("../validation/usersValidation");
+const { validateSignUp, validateSignIn } = require('../validation/usersValidation');
 
 const userSignUp = async (userDetails, role, res) => {
   try {
@@ -26,14 +23,14 @@ const userSignUp = async (userDetails, role, res) => {
     if (!emailNotTaken) {
       return res.json({
         status: 401,
-        success: "false",
-        message: `Email is already Registered. Try to Login`
+        success: 'false',
+        message: `Email is already Registered. Try to Login`,
       });
     }
 
     // check if confirm password is same as password
     if (!userDetails.confirmPassword === userDetails.password) {
-      return res.status(404).send("confirmed password is incorrect");
+      return res.status(404).send('confirmed password is incorrect');
     }
 
     // all coming from req.body
@@ -45,12 +42,12 @@ const userSignUp = async (userDetails, role, res) => {
     const hashPassword = await bcrypt.hash(password, salt);
 
     let user;
-    if (role === "patient") {
+    if (role === 'patient') {
       user = new User({
         name: name,
         email: email,
         password: hashPassword,
-        role: role
+        role: role,
       });
 
       const patientInformation = new Patient({
@@ -59,9 +56,8 @@ const userSignUp = async (userDetails, role, res) => {
         weight: userDetails.weight,
         bloodType: userDetails.bloodType,
         education: userDetails.education,
-        diseaseDescription: userDetails.diseaseDescription
+        diseaseDescription: userDetails.diseaseDescription,
         //! remember to add here after patient model is created
-
       });
 
       user.patientInfo = patientInformation;
@@ -69,12 +65,12 @@ const userSignUp = async (userDetails, role, res) => {
 
       // remove the unwanted material from the document
       delete user.doctorInfo;
-    } else if (role === "doctor") {
+    } else if (role === 'doctor') {
       user = new User({
         name: name,
         email: email,
         password: hashPassword,
-        role: role
+        role: role,
       });
 
       const doctorInformation = new Doctor({
@@ -100,12 +96,12 @@ const userSignUp = async (userDetails, role, res) => {
 
     await user.save();
 
-    const token = jwtGenerator(user)
+    const token = jwtGenerator(user);
 
     return res.status(201).json({
-      status: "success",
+      status: 'success',
       data: user,
-      token: token
+      token: token,
     });
   } catch (err) {
     console.log(err);
@@ -125,14 +121,10 @@ const userSignIn = async (userDetails, role, res) => {
     const { email, password } = userDetails;
 
     let user;
-    if (role === "patient") {
-      user = await User.findOne({ email })
-        .select("-doctorInfo")
-        .populate("patientInfo");
-    } else if (role === "doctor") {
-      user = await User.findOne({ email })
-        .select("-patientInfo")
-        .populate("doctorInfo");
+    if (role === 'patient') {
+      user = await User.findOne({ email }).select('-doctorInfo').populate('patientInfo');
+    } else if (role === 'doctor') {
+      user = await User.findOne({ email }).select('-patientInfo').populate('doctorInfo');
     }
 
     if (!user) {
@@ -145,26 +137,24 @@ const userSignIn = async (userDetails, role, res) => {
 
     if (user.role !== role) {
       return res.json({
-        message: "Please make sure you are logging in from the right portal.",
-        status: 403
+        message: 'Please make sure you are logging in from the right portal.',
+        status: 403,
       });
     }
 
     const isMatchPassword = await bcrypt.compare(password, user.password);
 
-    if (!isMatchPassword)
-      return res.json({ status: 400, message: "Password is incorrect" });
+    if (!isMatchPassword) return res.json({ status: 400, message: 'Password is incorrect' });
 
-    const token = jwtGenerator(user)
+    const token = jwtGenerator(user);
 
     //TODO: Remove password from return object
     // delete user["password"];//not working
 
     return res.json({
       status: 200,
-      data: user,
       token: token,
-      message: "Login Successful"
+      message: 'Login Successful',
     });
   } catch (err) {
     console.log(err.message);
@@ -179,5 +169,5 @@ const checkEmail = async (email) => {
 
 module.exports = {
   userSignUp,
-  userSignIn
+  userSignIn,
 };
