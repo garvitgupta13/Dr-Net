3b450191447773b5330146135db45b2cbb0c46ef
garvitgupta13 <garvitgupta2001@gmail.com>
2021-09-04 20:29:28 +0530

    adding user role to jwtToken

--- backend/utils/jwtGenerator.js
+++ backend/utils/jwtGenerator.js
@@ -1,13 +1,13 @@
-const jwt = require("jsonwebtoken");
-require('dotenv').config()
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 function jwtGenerator(user) {
-    const payload = {
-        _id : user._id,
-        name : user.name
-    }
-    return jwt.sign(payload, process.env.JWT_SECRET_KEY , { expiresIn: 31556926},)
+  const payload = {
+    _id: user._id,
+    name: user.name,
+    role: user.role,
+  };
+  return jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: 31556926 });
 }
-
 
 module.exports = jwtGenerator;
