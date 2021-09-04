8e8cf78fa88b09fdeb3c26d70108edef55d702c2
garvitgupta13 <garvitgupta2001@gmail.com>
2021-09-05 01:52:53 +0530

    fetching the chats from backend

--- frontend/src/Services/chatService.js
+++ frontend/src/Services/chatService.js
@@ -1,11 +1,23 @@
 import axios from 'axios';
 
-export const getConversations = async (token) => {
+export const getConversations = async () => {
     try {
-        axios.defaults.headers.common['Authorization'] = token;
+        axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
         const response = await axios.get(`${process.env.REACT_APP_API_ENDPOINT}/conversation`);
         return response;
     } catch (error) {
         console.log(error);
+        return error;
+    }
+};
+
+export const getMessages = async (conversationId) => {
+    try {
+        axios.defaults.headers.common['Authorization'] = localStorage.getItem('token');
+        const response = await axios.get(`${process.env.REACT_APP_API_ENDPOINT}/messages/${conversationId}`);
+        return response;
+    } catch (error) {
+        console.log(error);
+        return error;
     }
 };
--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -1,4 +1,4 @@
-import React,{useState, useCallback} from 'react';
+import React, { useState, useCallback, useContext, useEffect } from 'react';
 import Container from '@material-ui/core/Container';
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
@@ -7,165 +7,133 @@ import CardHeader from '@material-ui/core/CardHeader';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import {makeStyles} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
+import ChatContext from './Contexts/chatContext';
+import { getMessages } from '../Services/chatService';
+import { getCurrentUser } from './../Services/authService';
 
-const dummyData = [
-  {
-    name:'Sabo'
-  },
-  {
-    name:'Naomi'
-  },
-  {
-    name:'Zoro'
-  },
-  {
-    name:'Monkey D Luffy'
-  },
-  {
-    name:'Hawkeye'
-  },
-]
-
-const dummyMessages = [
-    {
-      sender:'Dr.Luthra',
-      message:"Hello Sabo what's your problem",
-    },
-    {
-      sender:'Sabo',
-      message:"My pain is unexpressable",
-    },
-    {
-      sender:'Dr.Luthra',
-      message:"How can I cure you when I don't know the cause",
-    },
-    {
-      sender:"Sabo",
-      message:"Are you sure!! You can cure my pain ....finally someone...",
-    },
-    {
-       sender:"Dr.Luthra",
-       message:"Ya sure go on tell me your problem",
-    },
-    {
-      sender:"Sabo",
-      message:"I am a young single and virgin boy ....snuffles.... I am in search of someone who could take my head ..blushes.. so  ..umgh... would you mind taking my head ",
-    },
-    {
-      sender:'Dr.Luthra',
-      message:"Hello Sabo what's your problem",
-    },
-    {
-      sender:'Sabo',
-      message:"My pain is unexpressable",
-    },
-    {
-      sender:'Dr.Luthra',
-      message:"How can I cure you when I don't know the cause",
-    },
-    {
-      sender:"Sabo",
-      message:"Are you sure!! You can cure my pain ....finally someone...",
-    },
-    {
-       sender:"Dr.Luthra",
-       message:"Ya sure go on tell me your problem",
-    },
-    {
-      sender:"Sabo",
-      message:"I am a young single and virgin boy ....snuffles.... I am in search of someone who could take my head ..blushes.. so  ..umgh... would you mind taking my head ",
-    },
-]
 const drawerWidth = 220;
-const useStyle = makeStyles(
-  {
-    'container':{
-      marginTop:"70px",
-      float:'left',
-      overflow:'auto',
-      height:'88vh',
-      paddingLeft:'5px',
-      ['@media (max-width:660px)']: {
-       marginLeft:"0px",
-     },
-     'main':{
-       backgroundColor: '#F9F3EC',
-       position:'relative',
-       marginTop:'20px',
-     },
-   },
+const useStyle = makeStyles({
+    container: {
+        marginTop: '70px',
+        float: 'left',
+        overflow: 'auto',
+        height: '88vh',
+        paddingLeft: '5px',
+        ['@media (max-width:660px)']: {
+            marginLeft: '0px',
+        },
+        main: {
+            backgroundColor: '#F9F3EC',
+            position: 'relative',
+            marginTop: '20px',
+        },
+    },
 });
 
-const Chat = ({width}) => {
+const Chat = ({ width, conversation }) => {
+    const classes = useStyle();
+    const [text, setText] = useState('');
+    const [messages, setMessages] = useState([]);
+    const [sender, setSender] = useState({});
+    const user = getCurrentUser();
+    const setRef = useCallback((node) => {
+        if (node) node.scrollIntoView({ smooth: true });
+    }, []);
 
-  console.log('width is ',width);
-  const classes = useStyle();
-  const [text,setText] = useState('');
-  const setRef = useCallback(node =>{
-     if(node)
-      node.scrollIntoView({smooth:true})
-  },[])
+    const submitHandler = (e) => {
+        e.preventDefault();
+        console.log('submitting bruh');
+        console.log(text);
+    };
 
-  const submitHandler = (e) => {
-    e.preventDefault();
-    console.log('submitting bruh');
-    console.log(text);
-  }
+    useEffect(() => {
+        if (conversation) {
+            getMessages(conversation._id)
+                .then(({ data, status }) => {
+                    if (status === 200) {
+                        setMessages(data);
+                        const msgSender =
+                            conversation.patient._id === user._id ? conversation.doctor : conversation.patient;
+                        setSender(msgSender);
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+    }, [conversation]);
+
+    if (!conversation) return null;
 
     return (
-         <div className={classes.container} style={{width:"100%"}}>
-               <Card className={classes.main} elevation={0} key={1}>
-                  <CardHeader
-                  avatar = {
-                    <Avatar style={{height:'45px',width:'45px'}}>
-                       {dummyData[0].name[0].toUpperCase()}
-                    </Avatar>
-                  }
-                  title={
-                  <div>
-                    <Typography gutterBottom variant="h6" component="h2" style={{color:'#936B3D'}}>
-                       {dummyData[0].name}
-                    </Typography>
-                  </div>
-                  }
-                  />
-               </Card>
-               <div style={{display:'flex', flexDirection:'column',flexGrow:'1'}}>
-                  <div style={{display:'flex',flexDirection:'column',alignItem:'start',justifyContent:'end'}}>
-                    {dummyMessages.map((message,index) => {
-                      const lastMessage = dummyMessages.length-1 === index
-                      const alignSelf = message.sender === 'Dr.Luthra' ? 'flex-end': '' ;
-                      const color = message.sender === 'Dr.Luthra' ? 'blue': 'white' ;
-                      const bgColor =  message.sender === 'Dr.Luthra' ? 'skyblue': 'white' ;
-                      const you = message.sender === 'Dr.Luthra' ? 'You': `${message.sender}` ;
-                      return(
-                        <div
-                        ref = {lastMessage?setRef:null}
-                        key={index}
-                        style={{display:'flex',flexDirection:'column',alignSelf:`${alignSelf}`}}
-                        >
-                            <div style={{ border:'1px solid',borderColor:`${color}`,backgroundColor:`${bgColor}`,padding:'10px',marginTop:'10px',borderRadius:'10px',fontSize:'17px' }}>
-                               {message.message}
+        <div className={classes.container} style={{ width: '100%' }}>
+            {sender.name ? (
+                <Card className={classes.main} elevation={0} key={1}>
+                    <CardHeader
+                        avatar={
+                            <Avatar style={{ height: '45px', width: '45px' }}>{sender.name[0].toUpperCase()}</Avatar>
+                        }
+                        title={
+                            <div>
+                                <Typography gutterBottom variant="h6" component="h2" style={{ color: '#936B3D' }}>
+                                    {sender.name}
+                                </Typography>
                             </div>
-                            <div style={{ alignSelf:`${alignSelf}`,fontSize:'15px'}}>
-                               {you}
+                        }
+                    />
+                </Card>
+            ) : (
+                <div></div>
+            )}
+            <div style={{ display: 'flex', flexDirection: 'column', flexGrow: '1' }}>
+                <div style={{ display: 'flex', flexDirection: 'column', alignItem: 'start', justifyContent: 'end' }}>
+                    {messages.map((message, index) => {
+                        let senderRole = messages.senderId === conversation.patient._id ? 'patient' : 'doctor';
+                        let lastMessage = messages.length - 1 === index;
+                        let alignSelf = message.senderId === user._id ? 'flex-end' : '';
+                        let color = message.senderId === user._id ? 'blue' : 'white';
+                        let bgColor = message.senderId === user._id ? 'skyblue' : 'white';
+                        let you = message.senderId === user._id ? 'You' : sender.name;
+
+                        return (
+                            <div
+                                ref={lastMessage ? setRef : null}
+                                key={index}
+                                style={{ display: 'flex', flexDirection: 'column', alignSelf: `${alignSelf}` }}
+                            >
+                                <div
+                                    style={{
+                                        border: '1px solid',
+                                        borderColor: `${color}`,
+                                        backgroundColor: `${bgColor}`,
+                                        padding: '10px',
+                                        marginTop: '10px',
+                                        borderRadius: '10px',
+                                        fontSize: '17px',
+                                    }}
+                                >
+                                    {message.text}
+                                </div>
+                                <div style={{ alignSelf: `${alignSelf}`, fontSize: '15px' }}>{you}</div>
                             </div>
-                        </div>
-                      )
-                      })
-                    }
-                  </div>
-               </div>
-               <form onSubmit = {submitHandler} style={{marginTop:'10px'}}>
-                  <textarea style={{width:'70%'}} rows="4" onChange={e=> setText(e.target.value)}>
-                  </textarea>
-                  <Button type="submit" color="secondary" variant="contained" style={{color:'#FFF3E5',width:'25%',marginLeft:'10px',marginTop:'-60px'}} >
-                      Submit
-                  </Button>
-               </form>
-      </div>
-    )
+                        );
+                    })}
+                </div>
+            </div>
+            <form onSubmit={submitHandler} style={{ marginTop: '10px' }}>
+                <textarea style={{ width: '70%' }} rows="4" onChange={(e) => setText(e.target.value)}></textarea>
+                <Button
+                    type="submit"
+                    color="secondary"
+                    variant="contained"
+                    style={{ color: '#FFF3E5', width: '25%', marginLeft: '10px', marginTop: '-60px' }}
+                >
+                    Send
+                </Button>
+            </form>
+        </div>
+    );
 };
 
 export default Chat;
