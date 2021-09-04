02add7bb44f152430c2f8ee69a26888c6c8a675f
garvitgupta13 <garvitgupta2001@gmail.com>
2021-09-05 01:52:18 +0530

    adding the chatContext and passing the current conversation to Chat component

--- frontend/src/components/RoadToChat.js
+++ frontend/src/components/RoadToChat.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UserList from './UserList.js';
 import Chat from './Chat.js';
+import ChatContext from './Contexts/chatContext';
 
-const RoadToChat = () =>{
-
-     const width = 32;
-     return(
-      <div style={{display:'flex',height:'100%'}}>
-         <UserList width={width}/>
-         <Chat width={100-width}/>
-      </div>
-     );
-}
+const RoadToChat = () => {
+    const [currentConversation, setCurrentConversation] = useState(null);
+    const handleConversation = (conversation) => {
+        setCurrentConversation(conversation);
+    };
+    const width = 32;
+    return (
+        <ChatContext.Provider value={{ currentConversation, handleConversation }}>
+            <div style={{ display: 'flex', height: '100%' }}>
+                <UserList width={width} />
+                <Chat width={100 - width} conversation={currentConversation} />
+            </div>
+        </ChatContext.Provider>
+    );
+};
 
 export default RoadToChat;
