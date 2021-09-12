331aa31c273df1d2629fa11a1fd2e43908aa9c40
garvitgupta13 <garvitgupta2001@gmail.com>
2021-09-12 18:26:25 +0530

    removing the appointment and chat module

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,29 @@
-const Express = require("express");
+const Express = require('express');
 const app = Express();
-const morgan = require("morgan");
-const cors = require("cors");
-const usersRoute = require("./routes/usersRoute");
-const patientRoute = require("./routes/patientRoute");
-const doctorRoute = require("./routes/doctorRoutes");
-const appointmentRoute = require("./routes/appointmentRoutes")
-const chatRoute = require("./routes/chatRoutes");
-const paymentRoute = require("./routes/paymentRoutes");
-const conversationRoute = require("./routes/conversationRoutes");
-const messageRoutes = require("./routes/messageRoutes");
-const bodyParser = require("body-parser");
+const morgan = require('morgan');
+const cors = require('cors');
+const usersRoute = require('./routes/usersRoute');
+const patientRoute = require('./routes/patientRoute');
+const doctorRoute = require('./routes/doctorRoutes');
+const paymentRoute = require('./routes/paymentRoutes');
+const conversationRoute = require('./routes/conversationRoutes');
+const messageRoutes = require('./routes/messageRoutes');
+const bodyParser = require('body-parser');
 
 //Connecting to DB
-require("./startup/db")();
+require('./startup/db')();
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE');
     return res.status(200).json({});
   }
   next();
@@ -37,18 +32,16 @@ app.use((req, res, next) => {
 //! Routes
 // require("./startup/routes")(app);
 
-app.use("/api/users", usersRoute);
-app.use("/api/patient", patientRoute);
-app.use("/api/doctor", doctorRoute);
-app.use("/api/appointment", appointmentRoute)
-app.use("/api/chat", chatRoute);
-app.use("/api/payment", paymentRoute)
-app.use("/api/conversation", conversationRoute);
-app.use("/api/messages", messageRoutes);
+app.use('/api/users', usersRoute);
+app.use('/api/patient', patientRoute);
+app.use('/api/doctor', doctorRoute);
+app.use('/api/payment', paymentRoute);
+app.use('/api/conversation', conversationRoute);
+app.use('/api/messages', messageRoutes);
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
-  console.log("Listening on port", port, "...");
+  console.log('Listening on port', port, '...');
 });
 
 module.exports = server;
