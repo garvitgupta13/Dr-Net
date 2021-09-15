8834118ef36f51cde2b068c7fdfbb802baa413d8
Kartik Singh Chuphal <64568294+kartik077-cr7@users.noreply.github.com>
2021-09-15 17:56:48 +0530

    Nothing much

--- frontend/src/Pages/PatientInfo.js
+++ frontend/src/Pages/PatientInfo.js
@@ -94,7 +94,8 @@ const PatientInfo = () => {
     }
 
     if (!patient) {
-        return <p className="centered focus">No Such Patient Exist</p>;
+        return (
+          <p className="centered focus">No Such Patient Exist</p>);
     }
 
     const handleChange = () => {
@@ -212,85 +213,5 @@ const PatientInfo = () => {
         </Container>
     );
   }
-
-  if (!patient) {
-    return <p className='centered focus'>No Such Patient Exist</p>;
-  }
-
-  const handleChange = () => {
-    setCanChange(prevState => !prevState);
-  }
-
-  const medHistory = patient.patientInfo.medicalHistory;
-
-  return (
-    <Container className={classes.container}>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={12} lg={6} key="01" className={classes.underGrid}>
-          <Card className={classes.main} elevation={5}>
-            <CardHeader
-              avatar={
-                <Avatar style={{ height: '90px', width: '90px' }} className={classes.Avatar}>
-                  {patient.name[0].toUpperCase()}
-                </Avatar>
-              }
-
-              title={
-                <div>
-                  <Typography gutterBottom variant="h3" component="h2" style={{ color: '#936B3D', marginLeft: '10px', fontSize: '35px' }}>
-                    {patient.name}
-                  </Typography>
-                </div>
-              }
-            />
-          </Card>
-          <Card className={classes.main} style={{ position: "relative" }} elevation={5}>
-            {!canChange && <CreateIcon onClick={handleChange} style={{ position: 'absolute', right: '40px', top: '20px' }} />}
-            {canChange && <SaveIcon onClick={handleChange} style={{ position: 'absolute', right: '40px', top: '20px' }} />}
-            <Typography variant="h5" component="h2" style={{ color: '#936B3D', textAlign: 'center', marginTop: '20px' }}>
-              More Information
-            </Typography>
-            <div style={{ marginTop: '10px', marginBottom: '100px', paddingLeft: '10px', paddingTop: '10px' }}>
-              <h3 className={classes.heading} style={{ marginTop: '40px' }}>Age :</h3>
-              <span className={classes.underheading} style={{ marginLeft: "53px" }}>{patient.patientInfo.age}</span>
-              <h3 className={classes.heading}>Height :</h3>
-              <span className={classes.underheading} style={{ marginLeft: "75px" }}>{patient.patientInfo.height}</span>
-              <h3 className={classes.heading}>Weight :</h3>
-              <span className={classes.underheading} style={{ marginLeft: "77px" }}>{patient.patientInfo.weight}</span>
-              <h3 className={classes.heading}>Blood Group :</h3>
-              <span className={classes.underheading} style={{ marginLeft: "125px" }}>{patient.patientInfo.bloodType}</span>
-              <h3 className={classes.heading}>Allergies :</h3>
-              <span className={classes.underheading} style={{ marginLeft: "95px" }}>{patient.patientInfo.diseaseDescription}</span>
-              <h3 className={classes.heading}>Marital Status :</h3>
-              <span className={classes.underheading} style={{ marginLeft: "135px" }}>Unmarried</span>
-              <h3 className={classes.heading}>Education :</h3>
-              <span className={classes.underheading} style={{ marginLeft: "105px" }}>{patient.patientInfo.education}</span>
-            </div>
-          </Card>
-        </Grid>
-        <Grid item xs={12} md={12} lg={6} key="02" className={classes.underGrid}>
-          <Card className={classes.main2} elevation={5}>
-            <Typography variant="h5" component="h2" style={{ color: '#936B3D', textAlign: 'center' }}>
-              Medical History
-            </Typography>
-            {
-              medHistory.map((history) => (
-                <MedicalHistory
-                  key={history._id}
-                  id={history._id}
-                  createdAt={history.createdAt}
-                  disease={history.disease}
-                  recommendedTests={history.recommendedTests}
-                  doctorId={history.doctorId}
-                  prescription={history.prescription}
-                />
-              ))
-            }
-          </Card>
-        </Grid>
-      </Grid>
-    </Container>
-  )
-}
 
 export default PatientInfo;
